1db6d95fe5476250551b38c8eeb4fc6bb49a507e
d-grossman <dgrossman@iqt.org>
2015-11-23 15:45:07 -0800

    making datatypes agree

--- LogSig/LogSig.py
+++ LogSig/LogSig.py
@@ -38,6 +38,7 @@ def match(X, S):
 
 
 def makeHash(s):
+
     m = hashlib.md5()
     m.update(s)
     return m.hexdigest()
@@ -55,27 +56,30 @@ def argMaxPhiSimple(C, X, G):
     # calculate the potential of X moving from cluster i to cluster j
     # using the simplified form *sum(r e R(X), p(r,Cj)^2 -p(r,Ci)^2)
     # using the cheat sum(p(r,Cdest))
+
     numGroups = len(C)
+
+    # see which group X should be in to maximize
+    currentGroup = G[makeHash(X)]
+
     retScore = 0.0
-    retval = 0.0
+    retval = currentGroup
 
     # make the tuples
     Xr = Counter(list(permutations(X.rstrip().split(), 2)))
 
-    # see which group X should be in to maximize
-    currentGroup = G[makeHash(X)]
     for nextGroup in range(numGroups):
-        currentScore = 0.0
 
         # dont consider transition to same group
         if nextGroup == currentGroup:
             continue
 
-        sum = 0.0
+        currentScore = 0.0
         for r in Xr:
-            sum = sum + C[nextGroup].get(r)
+            currentScore = currentScore + C[nextGroup].get(r)
+
         # TODO make sure this is the correct way to calculate
-        sum = sum / getWeight(C[nextGroup])
+        currentScore = currentScore / getWeight(C[nextGroup])
 
     # keep tabs of who is winning
     if retScore < currentScore:
@@ -84,36 +88,52 @@ def argMaxPhiSimple(C, X, G):
 
     return retval
 
+# store the data histograms
+# in each parition
+
 
 def randomSeeds(D, k):
-    C = [set() for _ in range(k)]
-    counter = 0
+    # g
+
+    C = [dict() for _ in range(k)]
+    partition = 0
     for d in D:
-        counter = (counter + 1) % k
-        # place log lines into sets
-        C[counter].add(d)
+        partition = (partition + 1) % k
+        # make histograms of loglines
+        Xr = Counter(list(permutations(d.strip().split(), 2)))
+
+        for key, count in Xr:
+            if key not in C[partition]:
+                C[partition][key] = 0
+            C[partition][key] = C[partition][key] + count
+
     return C
 
-    # TODO fill in the partition randomization funciton
-    return dict()
 
+def changePartition(C, X, G, i, j):
 
-def changePartition(C, X, G, i, jStar):
+    # move X from C[i] to C[j]
 
-    G[makeHash(X)] = jStar
+    # TODO would a binary version of this be sufficient?
+
+    G[makeHash(X)] = j
+
     Xr = Counter(list(permutations(X.rstrip().split(), 2)))
 
     for r, count in Xr:
+        # remove from i
         C[i][r] = C[i][r] - count
-        if r not in C[jStar]:
-            C[jStar][r] = 0
 
-        C[jStar][r] = C[jStar][r] + count
+        # add to j
+        if r not in C[j]:
+            C[j][r] = 0
+
+        C[j][r] = C[j][r] + count
 
 
-def listSetEqual(C, Clast):
+def listDictEqual(C, CLast):
     for i, s in enumerate(C):
-        if s != Clast[i]:
+        if s != CLast[i]:
             return False
     return True
 
@@ -123,8 +143,8 @@ def listSetEqual(C, Clast):
 # returns: C: log message partition
 def logSig_localSearch(D, k):
 
+    CLast = [dict() for _ in range(k)]
     C = randomSeeds(D, k)
-    CLast = [set() for _ in range(k)]
 
     # Create a map G to store messages group index
     G = dict()
@@ -133,12 +153,14 @@ def logSig_localSearch(D, k):
     # TODO see if this would be better to conserve memory
     # by making this a k way lookup
 
+    # TODO lookup to see if this always returns the same way
     for i, Ci in enumerate(C):
         for X in Ci:
             G[makeHash(X)] = i
 
-# TODO should this be an energy measure instead of dict?
-    while not listSetEqual(C, CLast):
+    # TODO should this be an energy measure
+    # instead of dict comp?
+    while not listDictEqual(C, CLast):
 
         CLast.deepcopy(C)
 
