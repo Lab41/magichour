6aedd254fcfa62ee0dc3a8be0128713a5ce716db
d-grossman <dgrossman@iqt.org>
2015-11-23 17:06:26 -0800

    main and a testfile

--- LogSig/LogSig.py
+++ LogSig/LogSig.py
@@ -13,17 +13,15 @@ def makeHash(s):
 
 # calculate the magnitude of a partition
 def getMagnitude(C):
-
     retval = 0
-    for r, count in C:
-        retval = retval + count
+    for key, count in C.iteritems():
+        retval = retval + int(count)
     return retval
 
 
 # calculate the best partition for X to be in
 # using the cheat sum(p(r,Cdest))
 def argMaxPhiSimple(C, X, G):
-
     numGroups = len(C)
 
     # see which group X should be in to maximize
@@ -43,7 +41,8 @@ def argMaxPhiSimple(C, X, G):
 
         currentScore = 0.0
         for r in Xr:
-            currentScore = currentScore + C[nextGroup].get(r)
+            if r in C[nextGroup]:
+                currentScore = currentScore + C[nextGroup].get(r)
 
         # TODO make sure this is the correct way to calculate
         currentScore = currentScore / getMagnitude(C[nextGroup])
@@ -58,19 +57,22 @@ def argMaxPhiSimple(C, X, G):
 
 # store the data histograms
 # in each parition
-def randomSeeds(D, k):
+def randomSeeds(D, k, G):
 
     C = [dict() for _ in range(k)]
     partition = 0
     for d in D:
         partition = (partition + 1) % k
         # make histograms of loglines
+        # TODO make sure that is is correct way of
+        # assigning groups to a message
+        G[makeHash(d)] = partition
         Xr = Counter(list(permutations(d.strip().split(), 2)))
 
-        for key, count in Xr:
+        for key, count in Xr.iteritems():
             if key not in C[partition]:
                 C[partition][key] = 0
-            C[partition][key] = C[partition][key] + count
+            C[partition][key] = C[partition][key] + int(count)
 
     return C
 
@@ -109,34 +111,49 @@ def listDictEqual(C, CLast):
 # returns: C: partitions
 def logSig_localSearch(D, k):
 
-    CLast = [dict() for _ in range(k)]
-    C = randomSeeds(D, k)
-
     # Create a map G to store messages group index
     G = dict()
+
+    CLast = [dict() for _ in range(k)]
+    C = randomSeeds(D, k, G)
 
     # place each logline into a set
     # TODO see if this would be better to conserve memory
     # by making this a k way lookup
 
     # TODO lookup to see if this always returns the same way
-    for i, Ci in enumerate(C):
-        for X in Ci:
-            G[makeHash(X)] = i
+    # for i, Ci in enumerate(C):
+    #    for X in Ci:
+    #        G[makeHash(X)] = i
 
     # TODO should this be an energy measure
     # instead of dict comp?
     while not listDictEqual(C, CLast):
+        print 'looping'
 
-        CLast.deepcopy(C)
+        CLast = C
 
         for Xj in D:
             i = G[makeHash(Xj)]
-            jStar = argMaxPhiSimple(C, X, G)
-
+            jStar = argMaxPhiSimple(C, Xj, G)
+            print 'i = %i, jStar= %i' % (i, jStar)
             if i != jStar:
+                print 'moving %s from %i to %i' % (Xj, i, jStar)
                 changePartition(C, Xj, G, i, jStar)
             # endif
         # endfor
     # end while
     return C
+
+
+def main():
+
+    a = open('testFiles/logFile', 'r')
+    D = a.readlines()
+    out = logSig_localSearch(D, 3)
+    for i in out:
+        print ('output %s' % i)
+
+
+if __name__ == "__main__":
+    main()
