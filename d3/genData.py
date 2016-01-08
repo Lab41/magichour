4930c929c4c2e813c51451439f5ce79907695738
d-grossman <dgrossman@iqt.org>
2016-01-07 17:08:06 -0800

    adding in my d3

---
+++ d3/genData.py
@@ -0,0 +1,30 @@
+import sys
+
+def computeStays(left,right):
+    retval = 0
+    for r in right:
+        retval += left and right
+    return retval
+
+def computeExits(left, right, inner):
+    retval = right(inner) - (left and right(inner))
+    return retval
+
+
+def makeComparisons(left, right):
+    catagories = range(len(left))
+    for outer in catagories:
+        outLine = list()
+        for inner in catagories:
+            work = 0
+            if outer == inner:
+                work =computeStays(left[outer], right)
+            else:
+                work =computeExits(left[outer], right, inner)
+            outLine.append(work)
+
+def main(argv):
+    print 'main'
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
