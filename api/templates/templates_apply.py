f44fa67f1e02b0577c02fdf5cad334c08cc4482d
kylemvz <kylez@lab41.org>
2016-01-20 13:35:43 -0800

    Switched apply_templates to use TimedTemplate named tuple.

--- api/templates/templates_apply.py
+++ api/templates/templates_apply.py
@@ -1,13 +1,20 @@
+from collections import namedtuple
+
+TimedTemplate = namedtuple("TimedTemplate", ["t", "template_id"])
 # probably better to rewrite this using map so that it can be parallelized
 # -1 = did not match any templates
-def apply_templates(templates, lines):
-    template_ids = list()
-    for line in lines:
-        matched = False
+
+#lines = iterable of LogLines
+#templates = output of functions in templates.py
+def apply_templates(templates, loglines):
+    timed_templates = list()
+    for logline in loglines:
+        timed_template = None #matched = False
         for template in templates:
-            if template.match.match(line):
-                template_ids.append(template.id)
-                matched = True
-        if not matched:
-            template_ids.append(-1)
-    return template_ids
+            if template.match.match(logline.msg):
+                timed_template = TimedTemplate(logline.t, template.id)
+                break
+        if not timed_template:
+            timed_template = TimedTemplate(logline.t, -1)
+        timed_templates.append(timed_template)
+    return timed_templates
