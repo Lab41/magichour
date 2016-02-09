4af1d63e4f126bcb6f6c481acccaff113cce4629
d-grossman <dgrossman@iqt.org>
2016-02-08 17:35:09 -0800

    changes from testing

--- magichour/api/dist/events/eventGen.py
+++ magichour/api/dist/events/eventGen.py
@@ -1,10 +1,42 @@
+from collections import defaultdict
 from magichour.api.dist.window.window import windowRDD
 from magichour.api.dist.FPGrowth.FPGrowth import FPGrowthRDD
 
+def get_longest_sets_possible(input_sets):
+    def is_subset(main_set, item):
+        is_subset = False
+        for main_item in main_set:
+            if item.issubset(main_item):
+                is_subset = True
+        return is_subset
+    input_dict = defaultdict(set)
+    for i in input_sets:
+        input_dict[len(i)].add(i)
+
+    output_sets = set()
+    lengths = sorted(input_dict.keys(), reverse=True) # Largest first
+    for i in input_dict[lengths[0]]: # since they are all the longest length we know that they are good
+        output_sets.add(i)
+
+    for length in lengths[1:]:
+        for item in input_dict[length]:
+            if not is_subset(output_sets, item):
+                output_sets.add(item)
+    return output_sets
+
 
 def eventGenRDD(sc, transactions,
-             minSupport=0.2,
-             numPartitions=10,
-             windowLen=120):
+                minSupport=0.2,
+                numPartitions=10,
+                windowLen=120):
+    retval = list()
     windowed = windowRDD(sc, transactions, windowLen, False)
-    return FPGrowthRDD(windowed, minSupport, numPartitions)
+    temp = FPGrowthRDD(windowed, minSupport, numPartitions)
+
+    items = [frozenset(fi.items) for fi in temp]
+    pruned_items = list(get_longest_sets_possible(items))
+    for item in pruned_items:
+        line = ' '.join([str(i) for i in sorted(item, key=int) if i! = -1])
+        retval.append(line)
+
+    return retval
