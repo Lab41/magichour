e8a9e64e57e35e160a4bf9b6e752b53d2c595d11
Yonas Tesfaye <ymt123@alum.mit.edu>
2016-02-11 15:09:08 -0800

    Moving to DistributedLogLines

--- magichour/api/dist/events/eventGen.py
+++ magichour/api/dist/events/eventGen.py
@@ -1,4 +1,9 @@
 from collections import defaultdict
+import hdbscan
+
+from pyspark.mllib.feature import Word2Vec
+
+from magichour.api.local.util.namedtuples import Event
 from magichour.api.dist.window.window import windowRDD
 from magichour.api.dist.FPGrowth.FPGrowth import FPGrowthRDD
 
@@ -29,24 +34,53 @@ def get_longest_sets_possible(input_sets):
     return output_sets
 
 
-def eventGenRDD(sc, transactions,
+def event_gen_fp_growth(sc, log_lines,
                 minSupport=0.2,
                 numPartitions=10,
                 windowLen=120,
                 remove_junk_drawer=True):
     retval = list()
-    windowed = windowRDD(sc, transactions, windowLen, False)
+    windowed = windowRDD(sc, log_lines, windowLen, False)
     temp = FPGrowthRDD(windowed, minSupport, numPartitions).collect()
 
     items = [frozenset(fi.items) for fi in temp]
     pruned_items = list(get_longest_sets_possible(items))
     if remove_junk_drawer:
-        for item in pruned_items:
-            line = ' '.join([str(i) for i in sorted(item, key=int) if i != -1])
-            retval.append(line)
+        for item_id, item in enumerate(pruned_items):
+            event = Event(id=item_id, template_ids=[i for i in sorted(item, key=int) if i != -1])
+            retval.append(event)
     else:
-        for item in pruned_items:
-            line = ' '.join([str(i) for i in sorted(item, key=int)])
-            retval.append(line)
+        for item_id, item in enumerate(pruned_items):
+            event = Event(id=item_id, template_ids=[i for i in sorted(item, key=int)])
+            retval.append(event)
 
     return retval
+
+def event_gen_word2vec(sc, log_lines, window_size=60):
+    D = log_lines.map(lambda logline: (int(logline.ts/window_size), (logline.ts, logline.templateId)))\
+                .groupByKey()\
+                .map(lambda (window,loglines): [str(templateId) for (ts,templateId) in sorted(loglines)])
+
+    # Run Word2Vec
+    model = Word2Vec().setVectorSize(16).setSeed(42).fit(D)
+    model_vectors = model.getVectors()
+
+    # mapping dict_distrib
+    labels = []
+    vectors = []
+    for label, vector in model_vectors.items():
+        labels.append(label)
+        vectors.append(list(vector))
+
+    # Clsutering
+    output_events = defaultdict(list)
+    for i, val in enumerate(hdbscan.HDBSCAN(min_cluster_size=2).fit_predict(vectors)):
+        output_events[val].append(labels[i])
+
+    # Create event objects
+    events = []
+    for item in output_events:
+        event = Event(id=item, template_ids=map(int, output_events[item]))
+        if len(event.template_ids) > 0:
+            events.append(event)
+    return events
--- magichour/api/dist/sample/driver_EventGen.py
+++ magichour/api/dist/sample/driver_EventGen.py
@@ -1,13 +1,26 @@
 sc.addPyFile('magichour.zip')
 
-from magichour.api.dist.events.eventGen import eventGenRDD
+from magichour.api.dist.events.eventGen import event_gen_fp_growth, event_gen_word2vec
 
-t = 'hdfs://namenode/magichour/tbird.500.templateEvalRDD'
+logLineURI = 'hdfs://namenode/magichour/tbird.500.templateEvalRDD'
+outputPath = 'hdfs://namenode/magichour/tbird.500.eventsRDD'
 minSupport = 0.2
 numPartitions = 10
 windowLen = 120
-transactions = sc.pickleFile(t)
+log_lines = sc.pickleFile(logLineURI)
 
-test = eventGenRDD(sc, transactions, minSupport, numPartitions, windowLen)
+print 'FP Growth'
+fp_growth_events = event_gen_fp_growth(sc, log_lines, minSupport, numPartitions, windowLen)
+for event in fp_growth_events:
+    print event
 
-test
+
+print 'Word2Vec'
+word2vec_events = event_gen_word2vec(sc, log_lines,  window_size=60)
+for event in word2vec_events:
+    print event
+
+
+# Save to hdfs
+if outputPath:
+    sc.parallelize(word2vec_events).saveAsPickleFile(outputPath, batchSize=1)
