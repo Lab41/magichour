f651d80956e62f84fe787aa30af6a885fe882cc6
Yonas Tesfaye <ymt123@alum.mit.edu>
2016-02-12 08:31:06 -0800

    Splitting up reading log files from template generation

--- magichour/api/dist/sample/driver_TemplateGen.py
+++ magichour/api/dist/sample/driver_TemplateGen.py
@@ -1,12 +1,25 @@
 sc.addPyFile('magichour.zip')
 
-from magichour.api.dist.templates.templateGen import templateGenRDD
+from magichour.api.dist.templates.templateGen import gen_tamplate_from_logs
+from magichour.api.dist.preprocess.readLog import readLogRDD
 
-transformURI = 'hdfs://namenode/magichour/simpleTrans'
-logInURI = 'hdfs://namenode/magichour/tbird.500k.gz'
+transforms_URI = 'hdfs://namenode/magichour/simpleTrans'
+raw_log_URI = 'hdfs://namenode/magichour/tbird.500k.gz'
+template_output_URI = 'hdfs://namenode/magichour/templates'
 support = 1000
 
-test = templateGenRDD(sc, logInURI, transformURI, support)
+# Read in log file RDD
+# Note: You may want to set persistence to MEMORY_ONLY or MEMORY_AND_DISK_SER depending on data size
+preprocessed_log_rdd = read_logs_from_uri(sc,
+                                          raw_log_URI,
+                                          preprocess_log=True,
+                                          transforms_URI=transforms_URI).cache()
 
-for i in test:
+# Generate Tempaltes
+templates = gen_tamplate_from_logs(sc, raw_log_rdd, transformURI, support)
+
+# Persist to disk for subsequent Analysis
+templates.pickleFile(template_output_URI)
+
+for i in templates:
     print ' '.join(i)
--- magichour/api/dist/templates/templateGen.py
+++ magichour/api/dist/templates/templateGen.py
@@ -1,9 +1,17 @@
-from magichour.api.dist.preprocess.readLog import readLogRDD
+
 from magichour.api.dist.preprocess.preProcess import preProcessRDD
 from magichour.lib.LogCluster.LogCluster import log_cluster
 
+def read_logs_from_uri(sc, log_uri, preprocess_log=True, transforms_URI=None):
+    if preprocess_log and transformURI is None:
+        raise ValueError('Preprocessing requires transform URI to be specified')
 
-def templateGenRDD(sc, logInURI, transformURI, support):
-    rddLogs = readLogRDD(sc, logInURI)
-    pre_processedLogs = preProcessRDD(sc, transformURI, rddLogs)
-    return log_cluster(sc, pre_processedLogs, support)
+    raw_log_rdd = readLogRDD(sc, log_uri)
+    if not preprocess_log:
+        return raw_log_rdd
+    else:
+        return preProcessRDD(sc, transforms_URI, raw_log_rdd)
+
+
+def gen_tamplate_from_logs(sc, logline_rdd, support):
+    return log_cluster(sc, logline_rdd, support)
