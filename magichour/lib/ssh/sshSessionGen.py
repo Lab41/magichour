f19e8b14ceca8141094e169d895cea1a3592ee42
d-grossman <dgrossman@iqt.org>
2016-02-22 07:37:49 -0800

    format

--- magichour/lib/ssh/sshSessionGen.py
+++ magichour/lib/ssh/sshSessionGen.py
@@ -15,8 +15,12 @@ def main(argv):
                         session starts or completes')
     parser.add_argument('-a', '--address', dest='address', required=True,
                         help='address of machine to run ssh on')
-    parser.add_argument('-s', '--session', dest='session', required=True,
-                        help='session will last between [sessionlength/2, sessionlength] seconds')
+    parser.add_argument(
+        '-s',
+        '--session',
+        dest='session',
+        required=True,
+        help='session will last between [sessionlength/2, sessionlength] seconds')
     parser.add_argument('-l', '--log', dest='logOutput', required=True,
                         help='file to log to')
 
@@ -30,28 +34,27 @@ def main(argv):
     address = options.address
 
     sessionLength = int(options.session)
-    sessionLength = randint(sessionLength/2, sessionLength)
+    sessionLength = randint(sessionLength / 2, sessionLength)
 
-    command = commands[randint(0, len(commands)-1)]
+    command = commands[randint(0, len(commands) - 1)]
 
-
-
-    time.sleep(randint(0,delay))
-    s = 'Starting %s [%s]' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), command)
+    time.sleep(randint(0, delay))
+    s = 'Starting %s [%s]' % (
+        datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), command)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.load_system_host_keys()
     actualCommand = '%s;sleep %i;logout\n' % (command, sessionLength)
-    #ssh.connect(address,key_filename='/Users/dgrossman/.ssh/id_rsa')
+    # ssh.connect(address,key_filename='/Users/dgrossman/.ssh/id_rsa')
     ssh.connect(address)
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(actualCommand)
     outList = list()
     for line in ssh_stdout.read().splitlines():
-        outList.append('%s\n'%line)
+        outList.append('%s\n' % line)
     o = 'Ending %s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
     outFile = open(options.logOutput, 'aw')
-    outFile.write('%s %s'% (s,o))
+    outFile.write('%s %s' % (s, o))
     outFile.close()
 
 
